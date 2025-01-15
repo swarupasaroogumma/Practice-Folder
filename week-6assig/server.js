dc2c4bd1b7095e3e72cfb12e94f54d69d705a023
Gumma swarupa <gummaswarupa@gmail.com>
2025-01-16 00:05:33 +0530

    pratice files

--- week-6assig/server.js
+++ week-6assig/server.js
@@ -5,7 +5,7 @@ const app=express();
 app.use(express.json());;
 
 const users=[]
-
+const todos=[]
 app.post("/signup", function(req,res){
     const username=req.body.username;
     const password=req.body.password;
@@ -30,6 +30,38 @@ app.post("/signin", function(req,res){
         res.sendStatus(404).send("invalid credintals")
     }
 
+    
+})
+function auth(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    try {
+        const verifiedToken = jwt.verify(authHeader, JWT_SECRET);
+        req.username = verifiedToken.username;
+        next();
+    } catch (error) {
+        console.error("Token verification error:", error.message);
+        res.status(403).send("Invalid or expired token");
+    
+    }
+}
+
+
+app.post("/todos",auth,function(req,res){
+    const addtodo=req.body.addtodo
+    todos.push({ id: todos.length + 1, username: req.username, addtodo, completed: false });
+    res.send({ message: 'TODO added!' });
+})
+
+app.put("/todos/:id",auth,function(req,res){
+    const allow=todos.find( u => u.username === req.username && u.id === parseInt(req.params.id))
+    if(allow){
+        allow.completed=!allow.completed
+        res.send({ message: 'TODO updda!' });
+
+    }else {
+        res.status(404).send({ message: 'TODO not found!' });
+    }
     
 })
 
