54cdda62a2b8c422ac82c672143907b5c196d387
Gumma swarupa <gummaswarupa@gmail.com>
2025-02-03 21:29:17 +0530

    reference

---
+++ week9assig/roll-up/src/App.jsx
@@ -0,0 +1,119 @@
+//roll up
+
+import { createConnection } from "mongoose";
+import { createContext } from "react";
+import { useContext } from "react";
+import { useState } from "react";
+
+// import { useState } from "react";
+
+
+// function App(){
+
+//   return(
+//     <>
+//     <Lightapp/>
+
+//     </>
+//   )
+
+// }
+// function Lightapp(){
+//   const[bulbon,setbulbon]=useState(true)
+//   return(
+//     <>
+    
+//     <Bulbon bulbon={bulbon} />
+//     <Toggle bulbon={bulbon} setbulbon={setbulbon}/>
+
+//     </>
+//   )
+// }
+
+// function Bulbon({bulbon}){
+//   return(
+//     <>
+//     {bulbon ? "builbon" :"bulboff"}
+//     </>
+//   )
+// }
+
+// function Toggle({bulbon,setbulbon}){
+
+//   return(
+//     <>
+//     <button onClick={toggle}>Toggle</button>
+    
+//     </>
+//   )
+
+//   function toggle(){
+//     setbulbon(!bulbon)
+//     //se
+//   }
+// }
+
+
+// export default App;
+
+//above we are passing props it increases complexity   propdrilling  thats y use useContext -it contains  createcontext,provider and consumer
+const Bulbcontext=createContext();
+
+function App(){
+  const[light,off]=useState(true);
+
+  return(
+    <>
+    <Bulbcontext.Provider value={{light:light,off:off}}>
+
+    <Lightcomp/>
+
+    </Bulbcontext.Provider>
+   
+
+    </>
+    
+  )
+
+}
+
+function Lightcomp(){
+  
+
+  return(
+    <>
+   
+    <Bulbstate/>
+
+    <Toggle/>
+
+    </>
+  )
+}
+
+
+function Bulbstate(){
+  const{light}=useContext(Bulbcontext);
+  return(
+    <>
+    {light ? "bulbon":"bulboff"}
+
+    </>
+  )
+}
+
+function Toggle(){
+  const{light,off}=useContext(Bulbcontext);
+  function toggle(){
+    off(!light)
+  }
+  return(
+    <>
+    <button onClick={toggle}>submit</button>
+
+    </>
+  )
+}
+
+
+export default App;
