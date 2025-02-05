8e06c73774ea135f65eea3ae590e38e8277ec54a
Gumma swarupa <gummaswarupa@gmail.com>
2025-02-05 22:59:16 +0530

    custom hook

---
+++ week9assig/customhooks/hooks/useFetch.jsx
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+// export  function usePost(){
+//     const[post,setpost]=useState({})
+
+//   async function getposts() {
+
+//     const res=await fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     const json=await res.json();
+//     setpost(json)
+    
+
+
+    
+//   } 
+
+//     useEffect(() =>{
+//         //why we use function here because of useefffect function shouldnot 
+//         getposts();
+//     },[])
+
+//     return post.title;
+
+
+// }
+
+//here it is more generic than above
+
+export function useFetch(url){
+    const[postdeta,setpostdet]=useState({})
+    const[loading,setloading]=useState(true)
+  async function getdetails() {
+    setloading(true)
+    const res= await fetch(url);
+    const json=await res.json();
+    setpostdet(json)
+    setloading(false)
+    
+  }  
+
+    useEffect(() => {
+        getdetails()
+
+    }, [url])
+
+    return{
+        postdeta,loading
+    }
+
+
+
+}
---
+++ week9assig/customhooks/src/App.jsx
@@ -0,0 +1,55 @@
+// import { useState } from 'react'
+// //custom hook creation
+// function useValue(){
+//   const [count, setCount] = useState(0)
+// function incct(){
+//   setCount( c=> c+1)
+// }
+//   return {
+//     count:count,
+//     incct:incct
+//   }
+// }
+
+import { use } from "react";
+import  { useFetch } from "../hooks/useFetch";
+import { useState } from "react";
+
+// function App() {
+//   //destructing the array
+//   const{count,incct}=useValue()
+  
+//   return (
+//     <>
+//     <button onClick={incct}>increase{count}</button>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+//lets try to fetch the dta from backeng using our custom hook useFetch
+
+function App(){
+  const[currrentpost,setcurrentpost]=useState(1)
+  const{  postdeta,loading }=useFetch("https://jsonplaceholder.typicode.com/todos/" + currrentpost)
+  if(loading){
+    return <>
+    loading bacha.. jara wait some time
+    </>
+  }
+
+  return(
+    <>
+    <button onClick={() => {setcurrentpost(1)}}>1</button>
+    <button onClick={() => {setcurrentpost(2)}}>2</button>
+    <button onClick={() => {setcurrentpost(3)}}>3</button>
+    {JSON.stringify(postdeta.title)}
+    </>
+  )
+
+}
+
+export default App;
