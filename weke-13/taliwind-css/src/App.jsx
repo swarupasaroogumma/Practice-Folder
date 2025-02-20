33a682eb74a5b8c198a23d3a0f255c8169cb72ee
Gumma swarupa <gummaswarupa@gmail.com>
2025-02-20 23:18:48 +0530

    tailwind practice

--- weke-13/taliwind-css/src/App.jsx
+++ weke-13/taliwind-css/src/App.jsx
@@ -1,26 +1,21 @@
-
 import './App.css'
 
 function App() {
-  
-
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <h1 className="text-xl  bg-blue font-bold underline">
-    Hello world!
-  </h1>
-  <h1 className="text-xl  bg-blue font-bold underline">
-    Hello world!
-  </h1>
-  
-
-      
-        
+      <div className="md:flex w-full sm:flex flex-column w-full ">
+        <div className="bg-red-300 flex-3 flex items-center justify-center">
+          hi there
+        </div>
+        <div className="bg-blue-300 flex-1 flex items-center justify-center">
+          hi there
+        </div>
+        <div className="bg-green-300 flex-1 flex items-center justify-center">
+          hi there
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
