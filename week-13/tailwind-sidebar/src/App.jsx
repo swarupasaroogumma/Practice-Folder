5ffb29901ce344824ede75bbbaf8fab0279d0282
Gumma swarupa <122894612+swarupasaroogumma@users.noreply.github.com>
2025-02-25 18:29:46 +0530

    tailwind-sidebar1

--- week-13/tailwind-sidebar/src/App.jsx
+++ week-13/tailwind-sidebar/src/App.jsx
@@ -1,3 +1,4 @@
+import { Sidebar1 } from "./Sidebar1"
 
 
 
@@ -6,9 +7,8 @@ function App() {
 
   return (
     <>
-      <h1 className="text-4xl font-bold underline">
-      Hello world!
-    </h1>
+    <Sidebar1/>
+      
     </>
   )
 }
---
+++ week-13/tailwind-sidebar/src/Sidebar1.jsx
@@ -0,0 +1,20 @@
+export const Sidebar1 = () =>{
+    return <>
+    <div className="flex">
+
+        {/* <div className="  transition-all delay-1000  md:translate-x-0    -translate-x-96 w-96 h-screen"> */}
+        <div className="transition-all  ease-in-out  duration-1000 md:w-96 w-0 h-screen">
+            Sidedbar
+
+        </div>
+        <div className="bg-green-500 w-full h-screen"> 
+            components
+            
+        </div>
+
+    </div>
+
+
+
+    </>
+}
