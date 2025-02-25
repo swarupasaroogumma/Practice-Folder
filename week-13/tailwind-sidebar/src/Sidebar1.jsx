8919a9188b082498fe880ef6c9b7bda468977596
Gumma swarupa <122894612+swarupasaroogumma@users.noreply.github.com>
2025-02-25 21:19:05 +0530

    tailwind dark-mode  practice

--- week-13/tailwind-sidebar/src/Sidebar1.jsx
+++ week-13/tailwind-sidebar/src/Sidebar1.jsx
@@ -3,7 +3,7 @@ export const Sidebar1 = () =>{
     <div className="flex">
 
         {/* <div className="  transition-all delay-1000  md:translate-x-0    -translate-x-96 w-96 h-screen"> */}
-        <div className="transition-all  ease-in-out  duration-1000 md:w-96 w-0 h-screen">
+        <div className="transition-all  ease-in-out  duration-150 md:w-96 w-0 h-screen">
             Sidedbar
 
         </div>
