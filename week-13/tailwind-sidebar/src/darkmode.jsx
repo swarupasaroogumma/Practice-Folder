f8a6df6c9fcb38c110a4c3332aefa5dc2a388788
Gumma swarupa <122894612+swarupasaroogumma@users.noreply.github.com>
2025-02-25 21:28:53 +0530

    darkmode

--- week-13/tailwind-sidebar/src/darkmode.jsx
+++ week-13/tailwind-sidebar/src/darkmode.jsx
@@ -2,7 +2,7 @@ export function Darkmode(){
     return <>
     <div className="h-screen bg-white dark:bg-black">
       <h1 className="text-black dark:text-white">hi there</h1>
-      <button onClick={() => {document.querySelector("html").classList.toggle("dark")}} className="dark:text-white">Toggle</button>
+      <button onClick={() => {document.querySelector("html").classList.toggle("dark")}} className="dark:text-white">Togglee</button>
     </div>
 
 
